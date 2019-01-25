e3666676a19a7fb5efea7e431e44d98549c3ac3f
omg <213123@qq.com>
2019-01-25 17:30:01 +0800

    修改/github/fjxiaozifeng1/application/views/errors/cli/error_general.php

--- application/views/errors/cli/error_general.php
+++ application/views/errors/cli/error_general.php
@@ -1,4 +1,5 @@
 <?php
+.ant-btn-primary.disabled.active>a:only-child,.ant-btn-primary.disabled:active>a:only-child,.ant-btn-primary.disabled:focus>a:only-child,.ant-btn-primary.disabled:hover>a:only-child,.ant-btn-primary.disabled>a:only-child,.ant-btn-primary[disabled].active>a:only-child,.ant-btn-primary[disabled]:active>a:only-child,.ant-btn-primary[disabled]:focus>a:only-child,.ant-btn-primary[disabled]:hover>a:only-child,.ant-btn-primary[disabled]>a:only-child {
 }
 defined('BASEPATH') OR exit('No direct script access allowed');
 
