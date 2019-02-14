2581112c72253ecf3e9121553c0e31433f866241
omg <213123@qq.com>
2019-02-14 17:30:01 +0800

    修改/github/fjxiaozifeng1/application/config/profiler.php

--- application/config/profiler.php
+++ application/config/profiler.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | -------------------------------------------------------------------------
 | Profiler Sections
 | -------------------------------------------------------------------------
+background-color: #1295bf;
 | This file lets you determine whether or not various sections of Profiler
 | data are displayed when the Profiler is enabled.
 | Please see the user guide for info:
