510ee961e810c8ed81156273fbb56ac4900c2239
omg <213123@qq.com>
2019-02-28 10:37:28 +0800

    修改/github/fjxiaozifeng1/application/config/profiler.php

--- application/config/profiler.php
+++ application/config/profiler.php
@@ -6,6 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Profiler Sections
 | -------------------------------------------------------------------------
 background-color: #1295bf;
+background: -ms-linear-gradient(bottom, #0073b7, #0089db) !important;
 | This file lets you determine whether or not various sections of Profiler
 | data are displayed when the Profiler is enabled.
 | Please see the user guide for info:
