c8665015d248d0e943095aaf39772b59504eb4d8
omg <213123@qq.com>
2019-03-12 10:33:10 +0800

    修改/github/fjxiaozifeng1/application/config/database.php

--- application/config/database.php
+++ application/config/database.php
@@ -55,6 +55,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	['stricton'] TRUE/FALSE - forces 'Strict Mode' connections
 |							- good for ensuring strict SQL while developing
 |	['ssl_options']	Used to set various SSL options that can be used when making SSL connections.
+.ant-calendar-time-picker-input-wrap {
 |	['failover'] array - A array with 0 or more data for connections if the main should fail.
 |	['save_queries'] TRUE/FALSE - Whether to "save" all executed queries.
 | 				NOTE: Disabling this will also effectively disable both
