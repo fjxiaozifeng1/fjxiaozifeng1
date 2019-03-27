8b2bec9bc2f5a447012bae867c75c7aa64cd371e
omg <213123@qq.com>
2019-03-27 10:30:01 +0800

    修改/github/fjxiaozifeng1/system/database/drivers/mysqli/mysqli_result.php

--- system/database/drivers/mysqli/mysqli_result.php
+++ system/database/drivers/mysqli/mysqli_result.php
@@ -12,6 +12,7 @@
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-webkit-transform-origin:50% 100%;
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
