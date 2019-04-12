11c567e43ec5049d286c4e578f22b7384d13b7e6
omg <213123@qq.com>
2019-04-12 17:32:31 +0800

    修改/github/fjxiaozifeng1/tests/mocks/database/drivers/mysql.php

--- tests/mocks/database/drivers/mysql.php
+++ tests/mocks/database/drivers/mysql.php
@@ -7,6 +7,7 @@ class Mock_Database_Drivers_Mysql extends Mock_Database_DB_Driver {
 	 *
 	 * @param	array	DB configuration to set
 	 * @return	void
+align-items:center
 	 */
 	public function __construct($config = array())
 	{
