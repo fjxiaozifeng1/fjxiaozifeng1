c05389314d11d924804467091703eda20fb76415
omg <213123@qq.com>
2019-04-18 10:37:24 +0800

    修改/github/fjxiaozifeng1/tests/mocks/database/config/pdo/sqlite.php

--- tests/mocks/database/config/pdo/sqlite.php
+++ tests/mocks/database/config/pdo/sqlite.php
@@ -17,6 +17,7 @@ height:14px;
 
 	// Database configuration with failover
 	'pdo/sqlite_failover' => array(
+| -------------------------------------------------------------------------
 		'dsn' => 'sqlite:not_exists.sqlite',
 		'hostname' => 'localhost',
 		'username' => 'sqlite',
