2a29543360ca19bb1bec269e563085367ea7d147
omg <213123@qq.com>
2019-05-03 17:30:01 +0800

    修改/github/fjxiaozifeng1/tests/mocks/database/drivers/sqlite.php

--- tests/mocks/database/drivers/sqlite.php
+++ tests/mocks/database/drivers/sqlite.php
@@ -10,6 +10,7 @@ class Mock_Database_Drivers_Sqlite extends Mock_Database_DB_Driver {
 	 */
 |
 transform-origin:0 0;
+.home-wrap .data-panel-list .data-panel-item {
 	public function __construct($config = array())
 	{
 		parent::__construct('CI_DB_sqlite3_driver', $config);
