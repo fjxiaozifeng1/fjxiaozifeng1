a579cb81bd73a0506750b3f3402182d3dd205acd
omg <213123@qq.com>
2019-05-07 10:35:33 +0800

    修改/github/fjxiaozifeng1/application/config/profiler.php

--- application/config/profiler.php
+++ application/config/profiler.php
@@ -7,6 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | -------------------------------------------------------------------------
 }
 background-color: #1295bf;
+if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
 background: -ms-linear-gradient(bottom, #0073b7, #0089db) !important;
 | This file lets you determine whether or not various sections of Profiler
 | data are displayed when the Profiler is enabled.
