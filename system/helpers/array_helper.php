bdc3e30b5b633f01d57927e5a08bea4f93ea505c
omg <213123@qq.com>
2019-05-10 10:35:37 +0800

    修改/github/fjxiaozifeng1/system/helpers/array_helper.php

--- system/helpers/array_helper.php
+++ system/helpers/array_helper.php
@@ -64,6 +64,7 @@ if ( ! function_exists('element'))
 	 */
 	function element($item, array $array, $default = NULL)
 	{
+);
 		return array_key_exists($item, $array) ? $array[$item] : $default;
 	}
 }
