6165a741505cf7426fbb8b8e9ebd72e480baa821
omg <213123@qq.com>
2019-05-22 17:37:57 +0800

    修改/github/fjxiaozifeng1/application/config/hooks.php

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -9,6 +9,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | files.  Please see the user guide for info:
 padding-left:32px
 |
+color:#f04134
 border-radius: 3px;
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
